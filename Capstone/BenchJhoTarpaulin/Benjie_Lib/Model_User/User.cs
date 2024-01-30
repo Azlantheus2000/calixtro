d3c65bdbf1c49fdc368726a8b2a1f01446121f85
unknown <calixtro.ajheu21@southernchristiancollege.edu.ph>
2024-01-31 02:30:16 +0800

    A

---
+++ Capstone/BenchJhoTarpaulin/Benjie_Lib/Model_User/User.cs
@@ -0,0 +1,15 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benjie_Lib.Model_User
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
